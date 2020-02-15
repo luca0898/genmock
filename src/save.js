01bdf685ae44b5f5f43d13d2856eb28a3a5718eb
NathanPB <nathan.pbombana@gmail.com>
2020-02-14 22:56:49 -0300

    Removed the stdout prints of the save function

--- src/save.js
+++ src/save.js
@@ -9,10 +9,8 @@ const fs = require("fs");
 function save(result, outFile = 'output.json') {
   fs.writeFile(outFile, result, "utf8", function(err) {
     if (err) {
-      console.log("An error occured while writing JSON Object to File.");
-      return console.log(err);
+      console.error("An error occurred while writing JSON Object to File.", err);
     }
-    console.log("JSON file has been saved.");
   });
 }
 
