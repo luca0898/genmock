13fb25bca55e4769d5808fcda0a603464bab970c
NathanPB <nathan.pbombana@gmail.com>
2020-02-14 23:32:32 -0300

    Created a high-order function to find the right function to output the data

--- src/output.js
+++ src/output.js
@@ -1,6 +1,22 @@
 const fs = require("fs");
 
 /**
+ * Finds the right output function to output the data.
+ *
+ * Empty ``outFile`` means that the data should be outputted to stdout
+ *
+ * @param {string | undefined} outFile - The path to the file to write the data
+ * @returns {(function(data): void)}
+ */
+function createOutputFunction(outFile) {
+  if (outFile) {
+    return result => saveToFile(result, outFile)
+  } else {
+    return writeToStdout
+  }
+}
+
+/**
  * Writes the data into a file
  *
  * @param {string} result - The data to write
@@ -14,4 +30,6 @@ function saveToFile(result, outFile = 'output.json') {
   });
 }
 
-module.exports = { save: saveToFile };
+const writeToStdout = console.log;
+
+module.exports = { save: saveToFile, createOutputFunction };
