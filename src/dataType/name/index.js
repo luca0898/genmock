d939fa1b88c62140434c95ebd67d9aed08a1b7f6
luca0898 <lucaguiraldello@hotmail.com>
2020-03-10 22:43:06 -0300

    adding 'faker' package

--- src/dataType/name/index.js
+++ src/dataType/name/index.js
@@ -1,13 +1,7 @@
-const LoremIpsum = require("lorem-ipsum").LoremIpsum;
+const faker = require("faker");
 
-module.exports = function ({ count }) {
-  const lorem = new LoremIpsum(count);
+module.exports = function({ locale }) {
+  if (locale) faker.locale = locale;
 
-  let firstName = lorem.generateWords(1);
-  let lastName = lorem.generateWords(1);
-
-  firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
-  lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
-
-  return `${firstName} ${lastName}`;
+  return faker.name.findName();
 };
